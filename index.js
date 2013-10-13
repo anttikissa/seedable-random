6acb7d12853ad1a9c0cf6304d7154f0b4ff7a548
Antti Sykäri <antti.sykari@gmail.com>
2013-10-13 04:38:23 +0300

    Add a Math.sin based random function.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-var seed = 0.5;
+var seed = new Date().getTime();
 
 function random() {
-	return seed;
+	// Avoid repeating when seeded by a number larger than the maximum integer
+	// representable in JavaScript (2^53).  
+	if (seed >= 9007199254740992) {
+		seed = 0;
+	}
+	var x = Math.sin(.8765111159592828 + seed++) * 10000.0;
+	return x - Math.floor(x);
 }
 
 random.seed = function(newSeed) {
